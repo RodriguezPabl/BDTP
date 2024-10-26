632b956ea81fe8801367cb1d8cfd9df7f25bc96e
Emanuel Aguilera <emaguilera@alumno.unlam.edu.ar>
2024-10-26 00:04:32 -0300

    Creacion de Tablas
    Creamos las tablas.

--- TP.sql
+++ TP.sql
@@ -29,54 +29,107 @@ de entrega, n�mero de grupo, nombre de la materia, nombres y DNI de los alumno
 Entregar todo en un zip cuyo nombre sea Grupo_XX.zip mediante la secci�n de pr�cticas de
 MIEL. Solo uno de los miembros del grupo debe hacer la entrega.
 */
+/*
+use master
 
-create database PruebaTP
+drop database Com2900G12
+*/
+create database Com2900G12
 go
 
-use PruebaTP
+use Com2900G12
 go
 
 -- ##### TABLAS #####
 -- Creación de esquemas
-CREATE SCHEMA Ventas
-CREATE SCHEMA Catalogo
-CREATE SCHEMA Empleados
+CREATE SCHEMA Ventas;
+go
+CREATE SCHEMA Catalogo;
+go
+CREATE SCHEMA Empleados;
+go
 
 -- Tabla de sucursales
-CREATE TABLE Ventas.Sucursales (
+CREATE TABLE Ventas.Sucursal (
     SucursalID INT PRIMARY KEY IDENTITY(1,1),
-    Nombre NVARCHAR(100) NOT NULL
+    Nombre NVARCHAR(100) NOT NULL,
+	Localidad NVARCHAR(100) NOT NULL, --ejemplo Sanjusto, Provincia de Buenos Aires
+	--codigo postal con check A1111
+	CodigoPostal NVARCHAR(5) NOT NULL CHECK (CodigoPostal LIKE '[A-Z][0-9][0-9][0-9][0-9]'),	
+	Direccion NVARCHAR(100) NOT NULL, --av geral paz 1234
+	--telefono con check  1111-1111
+	Telefono NVARCHAR(8) NOT NULL CHECK (Telefono LIKE '[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]'),
 );
+
+go
 
 -- Tabla de empleados
-CREATE TABLE Empleados.Empleados (
+CREATE TABLE Empleados.Empleado (
     EmpleadoID INT PRIMARY KEY IDENTITY(1,1),
     Nombre NVARCHAR(100) NOT NULL,
-    SucursalID INT,
-    CONSTRAINT FK_Sucursal FOREIGN KEY (SucursalID) REFERENCES Ventas.Sucursales(SucursalID),
-    Email NVARCHAR(100) NOT NULL,
-    CONSTRAINT UQ_Email UNIQUE (Email)
-);
+	Apellido NVARCHAR(100) NOT NULL,
+	--dni con check 11111111 98999999
+	Dni int NOT NULL CHECK (Dni BETWEEN 10000000 AND 99999999),
 
+	Localidad NVARCHAR(100) NOT NULL, --ejemplo Sanjusto, Provincia de Buenos Aires
+	--codigo postal con check A1111
+	CodigoPostal NVARCHAR(5) NOT NULL CHECK (CodigoPostal LIKE '[A-Z][0-9][0-9][0-9][0-9]'),	
+	Direccion NVARCHAR(100) NOT NULL, --av geral paz 1234
+
+    Email NVARCHAR(100) NOT NULL,
+	EmailEmpresarial NVARCHAR(100) NOT NULL,
+	Cuil NVARCHAR(100) NOT NULL,
+	Cargo NVARCHAR(100) NOT NULL,--ver si es una tabla aparte
+	SucursalID INT,
+	--turno TM, TT, TN, Jornada Completa
+	Turno NVARCHAR(16) NOT NULL CHECK (Turno IN ('TM', 'TT', 'TN', 'Jornada Completa')),--ver si es una tabla aparte
+    CONSTRAINT FK_Sucursal FOREIGN KEY (SucursalID) REFERENCES Ventas.Sucursal(SucursalID),
+    CONSTRAINT UQ_Email UNIQUE (Email),
+	CONSTRAINT UQ_EmailEmpresarial UNIQUE (EmailEmpresarial)
+);
+go
+--Tabla MedioDePago
+CREATE TABLE Ventas.MedioDePago (
+	MedioDePagoID INT PRIMARY KEY IDENTITY(1,1),
+	Nombre NVARCHAR(100) NOT NULL
+);
+go
 -- Tabla de productos
-CREATE TABLE Catalogo.Productos (
+CREATE TABLE Catalogo.Producto (
     ProductoID INT PRIMARY KEY IDENTITY(1,1),
+	LineaDeProducto NVARCHAR(100) NOT NULL,--ver si es una tabla aparte
     Nombre NVARCHAR(100) NOT NULL,
     Precio DECIMAL(10, 2) NOT NULL,
     SucursalID INT,
-    CONSTRAINT FK_Producto_Sucursal FOREIGN KEY (SucursalID) REFERENCES Ventas.Sucursales(SucursalID)
+    CONSTRAINT FK_Producto_Sucursal FOREIGN KEY (SucursalID) REFERENCES Ventas.Sucursal(SucursalID)
 );
-
+go
 -- Tabla de ventas
-CREATE TABLE Ventas.Ventas (
-    VentaID INT PRIMARY KEY IDENTITY(1,1),
-    EmpleadoID INT,
-    Fecha DATETIME NOT NULL DEFAULT GETDATE(),
+CREATE TABLE Ventas.Venta (
+	--ventaId 750-67-8428
+    VentaID NVARCHAR(100) PRIMARY KEY CHECK (VentaID LIKE '[0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]'), 
+	--TipoDeFactura A, B, C
+	TipoDeFactura NVARCHAR(1) NOT NULL CHECK (TipoDeFactura IN ('A', 'B', 'C')),
+	--ciudad si es Yangon reemplazar por San Justo
+	Ciudad NVARCHAR(100) NOT NULL,
+	TipoDeCliente NVARCHAR(100) NOT NULL, --ver si es una tabla aparte
+	Genero NVARCHAR(6) NOT NULL,
+	ProductoID INT,
+	PrecioUnitario DECIMAL(10, 2) NOT NULL,
+	Cantidad INT NOT NULL,
+    Fecha DATE NOT NULL DEFAULT GETDATE(),--Date tiene formato YYYY-MM-DD
+	Hora TIME NOT NULL,
+	MedioDePagoID INT,
+	IdentificadorDePago NVARCHAR(100) NOT NULL, --cvu / tarjeta de credito / efectivo --
+	EmpleadoID INT,
     Total DECIMAL(10, 2),
-    CONSTRAINT FK_Venta_Empleado FOREIGN KEY (EmpleadoID) REFERENCES Empleados.Empleados(EmpleadoID)
+    CONSTRAINT FK_Venta_Empleado FOREIGN KEY (EmpleadoID) REFERENCES Empleados.Empleado(EmpleadoID),
+	CONSTRAINT FK_Nombre_Producto FOREIGN KEY (ProductoID) REFERENCES Catalogo.Producto(ProductoID),
+	CONSTRAINT FK_MedioDePago FOREIGN KEY (MedioDePagoID) REFERENCES Ventas.MedioDePago(MedioDePagoID)
 );
-
+go
 -- Tabla de detalles de ventas
+/*
 CREATE TABLE Ventas.DetalleVentas (
     DetalleID INT PRIMARY KEY IDENTITY(1,1),
     VentaID INT,
@@ -86,13 +139,18 @@ CREATE TABLE Ventas.DetalleVentas (
     CONSTRAINT FK_Detalle_Venta FOREIGN KEY (VentaID) REFERENCES Ventas.Ventas(VentaID),
     CONSTRAINT FK_Detalle_Producto FOREIGN KEY (ProductoID) REFERENCES Catalogo.Productos(ProductoID)
 );
+*/
 
-select * from Ventas.Ventas
-select * from Ventas.DetalleVentas
-select * from Ventas.Sucursales
-select * from Catalogo.Productos
-select * from Empleados.Empleados
-
+select * from Ventas.Venta
+go
+--select * from Ventas.DetalleVentas
+select * from Ventas.Sucursal
+go
+select * from Catalogo.Producto
+go
+select * from Empleados.Empleado
+go
+/*
 -- ##### STORED PROCEDURES #####
 -- Procedimiento para insertar una nueva venta
 CREATE PROCEDURE InsertarVenta(
@@ -141,4 +199,4 @@ BEGIN
     FROM Ventas.Ventas
     WHERE MONTH(Fecha) = @Mes AND YEAR(Fecha) = @Anio
     GROUP BY DAY(Fecha);
-END;
+END;*/
