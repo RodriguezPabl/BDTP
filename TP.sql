cafe7c6981bddcdaad9e2e9b02b5d2aa165f494a
Pablo Rodriguez <103795541+RodriguezPabl@users.noreply.github.com>
2024-11-03 16:50:28 -0300

    Procedures de Insert, Update y Delete de todas las tablas
    Si vamos a cambiar algo de las tablas hay que acordarnos de cambiar también los procedures

--- TP.sql
+++ TP.sql
@@ -11,22 +11,22 @@
 
 /*
 Entrega 3
-Luego de decidirse por un motor de base de datos relacional, lleg� el momento de generar la
+Luego de decidirse por un motor de base de datos relacional, llegó el momento de generar la
 base de datos.
-Deber� instalar el DMBS y documentar el proceso. No incluya capturas de pantalla. Detalle
-las configuraciones aplicadas (ubicaci�n de archivos, memoria asignada, seguridad, puertos,
-etc.) en un documento como el que le entregar�a al DBA.
-Cree la base de datos, entidades y relaciones. Incluya restricciones y claves. Deber� entregar
-un archivo .sql con el script completo de creaci�n (debe funcionar si se lo ejecuta �tal cual� es
-entregado). Incluya comentarios para indicar qu� hace cada m�dulo de c�digo.
-Genere store procedures para manejar la inserci�n, modificado, borrado (si corresponde,
-tambi�n debe decidir si determinadas entidades solo admitir�n borrado l�gico) de cada tabla.
-Los nombres de los store procedures NO deben comenzar con �SP�.
-Genere esquemas para organizar de forma l�gica los componentes del sistema y aplique esto
-en la creaci�n de objetos. NO use el esquema �dbo�.
+Deberá instalar el DMBS y documentar el proceso. No incluya capturas de pantalla. Detalle
+las configuraciones aplicadas (ubicación de archivos, memoria asignada, seguridad, puertos,
+etc.) en un documento como el que le entregaría al DBA.
+Cree la base de datos, entidades y relaciones. Incluya restricciones y claves. Deberá entregar
+un archivo .sql con el script completo de creación (debe funcionar si se lo ejecuta “tal cual” es
+entregado). Incluya comentarios para indicar qué hace cada módulo de código.
+Genere store procedures para manejar la inserción, modificado, borrado (si corresponde,
+también debe decidir si determinadas entidades solo admitirán borrado lógico) de cada tabla.
+Los nombres de los store procedures NO deben comenzar con “SP”.
+Genere esquemas para organizar de forma lógica los componentes del sistema y aplique esto
+en la creación de objetos. NO use el esquema “dbo”.
 El archivo .sql con el script debe incluir comentarios donde consten este enunciado, la fecha
-de entrega, n�mero de grupo, nombre de la materia, nombres y DNI de los alumnos.
-Entregar todo en un zip cuyo nombre sea Grupo_XX.zip mediante la secci�n de pr�cticas de
+de entrega, número de grupo, nombre de la materia, nombres y DNI de los alumnos.
+Entregar todo en un zip cuyo nombre sea Grupo_XX.zip mediante la sección de prácticas de
 MIEL. Solo uno de los miembros del grupo debe hacer la entrega.
 */
 /*
@@ -52,20 +52,76 @@ go
 -- Tabla de sucursales
 CREATE TABLE Ventas.Sucursal (
     SucursalID INT PRIMARY KEY IDENTITY(1,1),
-    Nombre NVARCHAR(100) NOT NULL,
 	Localidad NVARCHAR(100) NOT NULL, --ejemplo Sanjusto, Provincia de Buenos Aires
 	--codigo postal con check A1111
 	CodigoPostal NVARCHAR(5) NOT NULL CHECK (CodigoPostal LIKE '[A-Z][0-9][0-9][0-9][0-9]'),	
 	Direccion NVARCHAR(100) NOT NULL, --av geral paz 1234
 	--telefono con check  1111-1111
-	Telefono NVARCHAR(8) NOT NULL CHECK (Telefono LIKE '[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]'),
+	Telefono NVARCHAR(9) NOT NULL CHECK (Telefono LIKE '[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]'),
 );
+go
 
+insert into Ventas.Sucursal (Localidad,CodigoPostal,Direccion,Telefono) values ('San Justo','B1754','Av. Brig. Gral. Juan Manuel de Rosas 3634','5555-5551'),
+																			('Ramos Mejia','B1704','Av. de Mayo 791','5555-5552'),
+																			('Lomas del Mirador','B1753',' Pres. Juan Domingo Perón 763','5555-5553')
+go
+
+CREATE PROCEDURE Ventas.ActualizarSucursal
+    @SucursalID INT,
+    @Localidad NVARCHAR(100) = NULL,
+    @CodigoPostal NVARCHAR(5) = NULL,
+    @Direccion NVARCHAR(100) = NULL,
+    @Telefono NVARCHAR(9) = NULL
+AS
+BEGIN
+	-- Verificar si la sucursal existe
+    IF NOT EXISTS (SELECT 1 FROM Ventas.Sucursal WHERE SucursalID = @SucursalID)
+    BEGIN
+        RAISERROR('Sucursal no encontrada.', 16, 1);
+        RETURN;
+    END
+
+	UPDATE Ventas.Sucursal
+    SET
+        Localidad = COALESCE(@Localidad, Localidad),
+        CodigoPostal = COALESCE(@CodigoPostal, CodigoPostal),
+        Direccion = COALESCE(@Direccion, Direccion),
+        Telefono = COALESCE(@Telefono, Telefono)
+    WHERE SucursalID = @SucursalID;
+END;
+go
+
+CREATE PROCEDURE Ventas.InsertarSucursal
+    @Localidad NVARCHAR(100),
+    @CodigoPostal NVARCHAR(5),
+    @Direccion NVARCHAR(100),
+    @Telefono NVARCHAR(9)
+AS
+BEGIN
+	INSERT INTO Ventas.Sucursal (Localidad, CodigoPostal, Direccion, Telefono)
+    VALUES (@Localidad, @CodigoPostal, @Direccion, @Telefono);
+END;
+go
+
+CREATE PROCEDURE Ventas.EliminarSucursal
+    @SucursalID INT
+AS
+BEGIN
+    -- Verificar si la sucursal existe
+    IF NOT EXISTS (SELECT 1 FROM Ventas.Sucursal WHERE SucursalID = @SucursalID)
+    BEGIN
+        RAISERROR('Sucursal no encontrada.', 16, 1);
+        RETURN;
+    END
+
+    DELETE FROM Ventas.Sucursal
+    WHERE SucursalID = @SucursalID;
+END;
 go
 
 -- Tabla de empleados
 CREATE TABLE Empleados.Empleado (
-    EmpleadoID INT PRIMARY KEY IDENTITY(1,1),
+    EmpleadoID INT PRIMARY KEY IDENTITY(257020,1),
     Nombre NVARCHAR(100) NOT NULL,
 	Apellido NVARCHAR(100) NOT NULL,
 	--dni con check 11111111 98999999
@@ -90,7 +146,84 @@ CREATE TABLE Empleados.Empleado (
 );
 go
 
---Tabla Cargo
+
+CREATE PROCEDURE Empleados.InsertarEmpleado
+    @Nombre NVARCHAR(100),
+    @Apellido NVARCHAR(100),
+    @Dni INT,
+    @Localidad NVARCHAR(100),
+    @CodigoPostal NVARCHAR(5),
+    @Direccion NVARCHAR(100),
+    @Email NVARCHAR(100),
+    @EmailEmpresarial NVARCHAR(100),
+    @Cuil NVARCHAR(100),
+    @Cargo NVARCHAR(100),
+    @SucursalID INT,
+    @Turno NVARCHAR(16)
+AS
+BEGIN
+    INSERT INTO Empleados.Empleado (Nombre, Apellido, Dni, Localidad, CodigoPostal, Direccion, Email, EmailEmpresarial, Cuil, Cargo, SucursalID, Turno)
+    VALUES (@Nombre, @Apellido, @Dni, @Localidad, @CodigoPostal, @Direccion, @Email, @EmailEmpresarial, @Cuil, @Cargo, @SucursalID, @Turno);
+END;
+go
+
+CREATE PROCEDURE Empleados.ActualizarEmpleado
+    @EmpleadoID INT,
+    @Nombre NVARCHAR(100) = NULL,
+    @Apellido NVARCHAR(100) = NULL,
+    @Dni INT = NULL,
+    @Localidad NVARCHAR(100) = NULL,
+    @CodigoPostal NVARCHAR(5) = NULL,
+    @Direccion NVARCHAR(100) = NULL,
+    @Email NVARCHAR(100) = NULL,
+    @EmailEmpresarial NVARCHAR(100) = NULL,
+    @Cuil NVARCHAR(100) = NULL,
+    @Cargo NVARCHAR(100) = NULL,
+    @SucursalID INT = NULL,
+    @Turno NVARCHAR(16) = NULL
+AS
+BEGIN
+    -- Verificar si el empleado existe
+    IF NOT EXISTS (SELECT 1 FROM Empleados.Empleado WHERE EmpleadoID = @EmpleadoID)
+    BEGIN
+        RAISERROR('Empleado no encontrado.', 16, 1);
+        RETURN;
+    END
+
+    -- Actualizar los campos solo si se proporciona un valor
+    UPDATE Empleados.Empleado
+    SET
+        Nombre = COALESCE(@Nombre, Nombre),
+        Apellido = COALESCE(@Apellido, Apellido),
+        Dni = COALESCE(@Dni, Dni),
+        Localidad = COALESCE(@Localidad, Localidad),
+        CodigoPostal = COALESCE(@CodigoPostal, CodigoPostal),
+        Direccion = COALESCE(@Direccion, Direccion),
+        Email = COALESCE(@Email, Email),
+        EmailEmpresarial = COALESCE(@EmailEmpresarial, EmailEmpresarial),
+        Cuil = COALESCE(@Cuil, Cuil),
+        Cargo = COALESCE(@Cargo, Cargo),
+        SucursalID = COALESCE(@SucursalID, SucursalID),
+        Turno = COALESCE(@Turno, Turno)
+    WHERE EmpleadoID = @EmpleadoID;
+END;
+go
+
+CREATE PROCEDURE Empleados.EliminarEmpleado
+    @EmpleadoID INT
+AS
+BEGIN
+    -- Verificar si el empleado existe
+    IF NOT EXISTS (SELECT 1 FROM Empleados.Empleado WHERE EmpleadoID = @EmpleadoID)
+    BEGIN
+        RAISERROR('Empleado no encontrado.', 16, 1);
+        RETURN;
+    END
+
+    DELETE FROM Empleados.Empleado
+    WHERE EmpleadoID = @EmpleadoID;
+END;
+go
 
 --Tabla MedioDePago
 CREATE TABLE Ventas.MedioDePago (
@@ -98,6 +231,51 @@ CREATE TABLE Ventas.MedioDePago (
 	Nombre NVARCHAR(100) NOT NULL
 );
 go
+
+CREATE PROCEDURE Ventas.InsertarMedioDePago
+    @Nombre NVARCHAR(100)
+AS
+BEGIN
+    INSERT INTO Ventas.MedioDePago (Nombre)
+    VALUES (@Nombre);
+END;
+go
+
+CREATE PROCEDURE Ventas.ActualizarMedioDePago
+    @MedioDePagoID INT,
+    @Nombre NVARCHAR(100) = NULL
+AS
+BEGIN
+    -- Verificar si el medio de pago existe
+    IF NOT EXISTS (SELECT 1 FROM Ventas.MedioDePago WHERE MedioDePagoID = @MedioDePagoID)
+    BEGIN
+        RAISERROR('Medio de pago no encontrado.', 16, 1);
+        RETURN;
+    END
+
+    -- Actualizar el nombre solo si se proporciona un valor
+    UPDATE Ventas.MedioDePago
+    SET Nombre = COALESCE(@Nombre, Nombre)
+    WHERE MedioDePagoID = @MedioDePagoID;
+END;
+go
+
+CREATE PROCEDURE Ventas.EliminarMedioDePago
+    @MedioDePagoID INT
+AS
+BEGIN
+    -- Verificar si el medio de pago existe
+    IF NOT EXISTS (SELECT 1 FROM Ventas.MedioDePago WHERE MedioDePagoID = @MedioDePagoID)
+    BEGIN
+        RAISERROR('Medio de pago no encontrado.', 16, 1);
+        RETURN;
+    END
+
+    DELETE FROM Ventas.MedioDePago
+    WHERE MedioDePagoID = @MedioDePagoID;
+END;
+go
+
 -- Tabla de productos
 CREATE TABLE Catalogo.Producto (
     ProductoID INT PRIMARY KEY IDENTITY(1,1),
@@ -109,9 +287,67 @@ CREATE TABLE Catalogo.Producto (
     SucursalID INT,
     CONSTRAINT FK_Producto_Sucursal FOREIGN KEY (SucursalID) REFERENCES Ventas.Sucursal(SucursalID)
 );
-
 go
 
+CREATE PROCEDURE Catalogo.InsertarProducto
+    @LineaDeProducto NVARCHAR(100),
+    @Nombre NVARCHAR(100),
+    @Precio DECIMAL(10, 2),
+    @PrecioReferencial DECIMAL(10, 2) = NULL,
+    @UnidadDeReferencia NVARCHAR(2) = NULL,
+    @SucursalID INT
+AS
+BEGIN
+    INSERT INTO Catalogo.Producto (LineaDeProducto, Nombre, Precio, PrecioReferencial, UnidadDeReferencia, SucursalID)
+    VALUES (@LineaDeProducto, @Nombre, @Precio, @PrecioReferencial, @UnidadDeReferencia, @SucursalID);
+END;
+go
+
+CREATE PROCEDURE Catalogo.ActualizarProducto
+    @ProductoID INT,
+    @LineaDeProducto NVARCHAR(100) = NULL,
+    @Nombre NVARCHAR(100) = NULL,
+    @Precio DECIMAL(10, 2) = NULL,
+    @PrecioReferencial DECIMAL(10, 2) = NULL,
+    @UnidadDeReferencia NVARCHAR(2) = NULL,
+    @SucursalID INT = NULL
+AS
+BEGIN
+    -- Verificar si el producto existe
+    IF NOT EXISTS (SELECT 1 FROM Catalogo.Producto WHERE ProductoID = @ProductoID)
+    BEGIN
+        RAISERROR('Producto no encontrado.', 16, 1);
+        RETURN;
+    END
+
+    -- Actualizar los campos solo si se proporciona un valor
+    UPDATE Catalogo.Producto
+    SET
+        LineaDeProducto = COALESCE(@LineaDeProducto, LineaDeProducto),
+        Nombre = COALESCE(@Nombre, Nombre),
+        Precio = COALESCE(@Precio, Precio),
+        PrecioReferencial = COALESCE(@PrecioReferencial, PrecioReferencial),
+        UnidadDeReferencia = COALESCE(@UnidadDeReferencia, UnidadDeReferencia),
+        SucursalID = COALESCE(@SucursalID, SucursalID)
+    WHERE ProductoID = @ProductoID;
+END;
+go
+
+CREATE PROCEDURE Catalogo.EliminarProducto
+    @ProductoID INT
+AS
+BEGIN
+    -- Verificar si el producto existe
+    IF NOT EXISTS (SELECT 1 FROM Catalogo.Producto WHERE ProductoID = @ProductoID)
+    BEGIN
+        RAISERROR('Producto no encontrado.', 16, 1);
+        RETURN;
+    END
+
+    DELETE FROM Catalogo.Producto
+    WHERE ProductoID = @ProductoID;
+END;
+go
 
 CREATE TABLE Catalogo.Catalogo (
     ProductoID INT PRIMARY KEY,
@@ -122,8 +358,69 @@ CREATE TABLE Catalogo.Catalogo (
     UnidadDeReferencia NVARCHAR(100),
     Fecha DATETIME
 );
-
 go
+
+CREATE PROCEDURE Catalogo.InsertarCatalogo
+    @ProductoID INT,
+    @LineaDeProducto NVARCHAR(100),
+    @Nombre NVARCHAR(100),
+    @Precio DECIMAL(10, 2),
+    @PrecioReferencial DECIMAL(10, 2) = NULL,
+    @UnidadDeReferencia NVARCHAR(100),
+    @Fecha DATETIME
+AS
+BEGIN
+    INSERT INTO Catalogo.Catalogo (ProductoID, LineaDeProducto, Nombre, Precio, PrecioReferencial, UnidadDeReferencia, Fecha)
+    VALUES (@ProductoID, @LineaDeProducto, @Nombre, @Precio, @PrecioReferencial, @UnidadDeReferencia, @Fecha);
+END;
+GO
+
+CREATE PROCEDURE Catalogo.ActualizarCatalogo
+    @ProductoID INT,
+    @LineaDeProducto NVARCHAR(100) = NULL,
+    @Nombre NVARCHAR(100) = NULL,
+    @Precio DECIMAL(10, 2) = NULL,
+    @PrecioReferencial DECIMAL(10, 2) = NULL,
+    @UnidadDeReferencia NVARCHAR(100) = NULL,
+    @Fecha DATETIME = NULL
+AS
+BEGIN
+    -- Verificar si el producto existe
+    IF NOT EXISTS (SELECT 1 FROM Catalogo.Catalogo WHERE ProductoID = @ProductoID)
+    BEGIN
+        RAISERROR('Producto no encontrado en el catálogo.', 16, 1);
+        RETURN;
+    END
+
+    -- Actualizar los campos solo si se proporciona un valor
+    UPDATE Catalogo.Catalogo
+    SET
+        LineaDeProducto = COALESCE(@LineaDeProducto, LineaDeProducto),
+        Nombre = COALESCE(@Nombre, Nombre),
+        Precio = COALESCE(@Precio, Precio),
+        PrecioReferencial = COALESCE(@PrecioReferencial, PrecioReferencial),
+        UnidadDeReferencia = COALESCE(@UnidadDeReferencia, UnidadDeReferencia),
+        Fecha = COALESCE(@Fecha, Fecha)
+    WHERE ProductoID = @ProductoID;
+END;
+GO
+
+CREATE PROCEDURE Catalogo.EliminarCatalogo
+    @ProductoID INT
+AS
+BEGIN
+    -- Verificar si el producto existe
+    IF NOT EXISTS (SELECT 1 FROM Catalogo.Catalogo WHERE ProductoID = @ProductoID)
+    BEGIN
+        RAISERROR('Producto no encontrado en el catálogo.', 16, 1);
+        RETURN;
+    END
+
+    DELETE FROM Catalogo.Catalogo
+    WHERE ProductoID = @ProductoID;
+END;
+GO
+
 -- Tabla de ventas
 CREATE TABLE Ventas.Venta (
 	--ventaId 750-67-8428
@@ -148,6 +445,86 @@ CREATE TABLE Ventas.Venta (
 	CONSTRAINT FK_MedioDePago FOREIGN KEY (MedioDePagoID) REFERENCES Ventas.MedioDePago(MedioDePagoID)
 );
 go
+
+CREATE PROCEDURE Ventas.InsertarVenta
+    @VentaID NVARCHAR(100),
+    @TipoDeFactura NVARCHAR(1),
+    @Ciudad NVARCHAR(100),
+    @TipoDeCliente NVARCHAR(100),
+    @Genero NVARCHAR(6),
+    @ProductoID INT,
+    @PrecioUnitario DECIMAL(10, 2),
+    @Cantidad INT,
+    @Hora TIME,
+    @MedioDePagoID INT,
+    @IdentificadorDePago NVARCHAR(100),
+    @EmpleadoID INT,
+    @Total DECIMAL(10, 2)
+AS
+BEGIN
+    INSERT INTO Ventas.Venta (VentaID, TipoDeFactura, Ciudad, TipoDeCliente, Genero, ProductoID, PrecioUnitario, Cantidad, Fecha, Hora, MedioDePagoID, IdentificadorDePago, EmpleadoID, Total)
+    VALUES (@VentaID, @TipoDeFactura, @Ciudad, @TipoDeCliente, @Genero, @ProductoID, @PrecioUnitario, @Cantidad, GETDATE(), @Hora, @MedioDePagoID, @IdentificadorDePago, @EmpleadoID, @Total);
+END;
+GO
+
+CREATE PROCEDURE Ventas.ActualizarVenta
+    @VentaID NVARCHAR(100),
+    @TipoDeFactura NVARCHAR(1) = NULL,
+    @Ciudad NVARCHAR(100) = NULL,
+    @TipoDeCliente NVARCHAR(100) = NULL,
+    @Genero NVARCHAR(6) = NULL,
+    @ProductoID INT = NULL,
+    @PrecioUnitario DECIMAL(10, 2) = NULL,
+    @Cantidad INT = NULL,
+    @Hora TIME = NULL,
+    @MedioDePagoID INT = NULL,
+    @IdentificadorDePago NVARCHAR(100) = NULL,
+    @EmpleadoID INT = NULL,
+    @Total DECIMAL(10, 2) = NULL
+AS
+BEGIN
+    -- Verificar si la venta existe
+    IF NOT EXISTS (SELECT 1 FROM Ventas.Venta WHERE VentaID = @VentaID)
+    BEGIN
+        RAISERROR('Venta no encontrada.', 16, 1);
+        RETURN;
+    END
+
+    -- Actualizar los campos solo si se proporciona un valor
+    UPDATE Ventas.Venta
+    SET
+        TipoDeFactura = COALESCE(@TipoDeFactura, TipoDeFactura),
+        Ciudad = COALESCE(@Ciudad, Ciudad),
+        TipoDeCliente = COALESCE(@TipoDeCliente, TipoDeCliente),
+        Genero = COALESCE(@Genero, Genero),
+        ProductoID = COALESCE(@ProductoID, ProductoID),
+        PrecioUnitario = COALESCE(@PrecioUnitario, PrecioUnitario),
+        Cantidad = COALESCE(@Cantidad, Cantidad),
+        Hora = COALESCE(@Hora, Hora),
+        MedioDePagoID = COALESCE(@MedioDePagoID, MedioDePagoID),
+        IdentificadorDePago = COALESCE(@IdentificadorDePago, IdentificadorDePago),
+        EmpleadoID = COALESCE(@EmpleadoID, EmpleadoID),
+        Total = COALESCE(@Total, Total)
+    WHERE VentaID = @VentaID;
+END;
+GO
+
+CREATE PROCEDURE Ventas.EliminarVenta
+    @VentaID NVARCHAR(100)
+AS
+BEGIN
+    -- Verificar si la venta existe
+    IF NOT EXISTS (SELECT 1 FROM Ventas.Venta WHERE VentaID = @VentaID)
+    BEGIN
+        RAISERROR('Venta no encontrada.', 16, 1);
+        RETURN;
+    END
+
+    DELETE FROM Ventas.Venta
+    WHERE VentaID = @VentaID;
+END;
+GO
+
 -- Tabla de detalles de ventas
 /*
 CREATE TABLE Ventas.DetalleVentas (
@@ -164,20 +541,16 @@ CREATE TABLE Ventas.DetalleVentas (
 select * from Ventas.Venta
 go
 --select * from Ventas.DetalleVentas
-select * from Ventas.Sucursal
-go
 select * from Catalogo.Producto
 go
 select * from Empleados.Empleado
 go
 
-
-
 --tabla para obtener el path del archivo .sql E:\Unlam\BaseDeDatosAplicada\2024-2C\TP\BDTP\TP.sql
 
 
 --Insertar datos de catalogo.csv from ../TP_integrador_Archivos\Productos\catalogo.csv
-
+/*
 BULK INSERT Catalogo.Catalogo
 FROM 'E:\Unlam\BaseDeDatosAplicada\2024-2C\TP\BDTP\TP_integrador_Archivos\Productos\catalogo.csv'  -- Ruta relativa
 WITH
@@ -281,5 +654,5 @@ BEGIN
     WHERE MONTH(Fecha) = @Mes AND YEAR(Fecha) = @Anio
     GROUP BY DAY(Fecha);
 END;*/
-
+*/
 
