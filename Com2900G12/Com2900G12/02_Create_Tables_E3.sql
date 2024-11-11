4a75ee05b75f36b0719bd544dffb192e59e0cbb9
Pablo Rodriguez <103795541+RodriguezPabl@users.noreply.github.com>
2024-11-11 20:05:17 -0300

    DER, Documentación, Consigna e Integrantes

--- Com2900G12/Com2900G12/02_Create_Tables_E3.sql
+++ Com2900G12/Com2900G12/02_Create_Tables_E3.sql
@@ -1,3 +1,33 @@
+/*
+	Bases de datos aplicada
+	Grupo 12
+	Integrantes:
+		-Rodriguez Pablo, 42949072
+		-Aguilera Emanuel, 41757402
+		-Tatiana Greve, 43031180
+		-Nogueira Denise, 41234014
+	Fecha: 12/11/24
+*/
+
+/* Entrega 3
+Luego de decidirse por un motor de base de datos relacional, llegó el momento de generar la
+base de datos.
+Deberá instalar el DMBS y documentar el proceso. No incluya capturas de pantalla. Detalle
+las configuraciones aplicadas (ubicación de archivos, memoria asignada, seguridad, puertos,
+etc.) en un documento como el que le entregaría al DBA.
+Cree la base de datos, entidades y relaciones. Incluya restricciones y claves. Deberá entregar
+un archivo .sql con el script completo de creación (debe funcionar si se lo ejecuta “tal cual” es
+entregado). Incluya comentarios para indicar qué hace cada módulo de código.
+Genere store procedures para manejar la inserción, modificado, borrado (si corresponde,
+también debe decidir si determinadas entidades solo admitirán borrado lógico) de cada tabla.
+Los nombres de los store procedures NO deben comenzar con “SP”.
+Genere esquemas para organizar de forma lógica los componentes del sistema y aplique esto
+en la creación de objetos. NO use el esquema “dbo”.
+El archivo .sql con el script debe incluir comentarios donde consten este enunciado, la fecha
+de entrega, número de grupo, nombre de la materia, nombres y DNI de los alumnos.
+Entregar todo en un zip cuyo nombre sea Grupo_XX.zip mediante la sección de prácticas de
+MIEL. Solo uno de los miembros del grupo debe hacer la entrega. */
+
 USE Com2900G12
 GO
 
--- Com2900G12/Com2900G12/06_Create_SP_Importacion_E4.sql
+++ Com2900G12/Com2900G12/06_Create_SP_Importacion_E4.sql
@@ -1,3 +1,33 @@
+/*
+	Bases de datos aplicada
+	Grupo 12
+	Integrantes:
+		-Rodriguez Pablo, 42949072
+		-Aguilera Emanuel, 41757402
+		-Tatiana Greve, 43031180
+		-Nogueira Denise, 41234014
+	Fecha: 12/11/24
+*/
+
+/* Entrega 4
+Se proveen maestros de XXX.
+Ver archivo “Datasets para importar” en Miel.
+Se requiere que importe toda la información antes mencionada a la base de datos:
+• Genere los objetos necesarios (store procedures, funciones, etc.) para importar los
+archivos antes mencionados. Tenga en cuenta que cada mes se recibirán archivos de
+novedades con la misma estructura, pero datos nuevos para agregar a cada maestro.
+• Considere este comportamiento al generar el código. Debe admitir la importación de
+novedades periódicamente.
+• Cada maestro debe importarse con un SP distinto. No se aceptarán scripts que
+realicen tareas por fuera de un SP.
+• La estructura/esquema de las tablas a generar será decisión suya. Puede que deba
+realizar procesos de transformación sobre los maestros recibidos para adaptarlos a la
+estructura requerida.
+• Los archivos CSV/JSON no deben modificarse. En caso de que haya datos mal
+cargados, incompletos, erróneos, etc., deberá contemplarlo y realizar las correcciones
+en el fuente SQL. (Sería una excepción si el archivo está malformado y no es posible
+interpretarlo como JSON o CSV). */
+
 use Com2900G12;
 GO
 
